03ab9c8053b1e275f159014a65875dc5446b9a3b
markr <markrouz@yandex.ru>
2018-10-01 20:39:34 +0700

    добавлена левая панель (требует доработки)

--- src/app/core/components/common-frame/common-frame.component.ts
+++ src/app/core/components/common-frame/common-frame.component.ts
@@ -7,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommonFrameComponent implements OnInit {
 
-  links = [
-    'jsdalfjsdafj',
-    'fsdfja;sldfj',
-    'fjwejfowegjwoj',
+  sidebarHeaders = [
+    {
+      header: 'Monitoring',
+      links: [
+        { name: 'Analytics', path: '/analytics', icon: 'src/assets/sidebar/dashboard.png' },
+        { name: 'Map', path: '/map', icon: 'src/assets/sidebar/map.png' },
+        { name: 'Events', path: '/events', icon: 'src/assets/sidebar/developments.png' },
+      ],
+    },
+    {
+      header: 'Reports',
+      links: [
+        { name: 'Reports SLA', path: '/reports_sla', icon: 'src/assets/sidebar/SLA-reports.png' },
+      ],
+    },
+    {
+      header: 'Infrastructure',
+      links: [
+        { name: 'Services', path: '/services', icon: 'src/assets/sidebar/services.png' },
+        { name: 'Contracts', path: '/contracts', icon: 'src/assets/sidebar/contracts.png' },
+        { name: 'Probes', path: '/probes', icon: 'src/assets/sidebar/probes.png' },
+      ],
+    },
+    {
+      header: 'Administration',
+      links: [
+        { name: 'Contractors', path: '/contractors', icon: 'src/assets/sidebar/contractors.png' },
+        { name: 'SLA', path: '/sla', icon: 'src/assets/sidebar/SLA.png' },
+        { name: 'SAP', path: '/sap', icon: 'src/assets/sidebar/access-point.png' },
+        { name: 'Tests', path: '/tests', icon: 'src/assets/sidebar/tests.png' },
+        { name: 'Accounts', path: '/accounts', icon: 'src/assets/sidebar/accounts.png' },
+        { name: 'Sessions', path: '/sessions', icon: 'src/assets/sidebar/session.png' },
+        { name: 'Event log', path: '/event_log', icon: 'src/assets/sidebar/event-log.png' },
+      ],
+    },
   ];
 
   constructor() { }
@@ -18,7 +49,4 @@ export class CommonFrameComponent implements OnInit {
   ngOnInit() {
   }
 
-  showInfo(link: string) {
-    console.log(link);
-  }
 }
